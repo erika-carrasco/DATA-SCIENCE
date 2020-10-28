6a433ea31b27c0d77a2f61363e169d2d72b90cc9
erika-carrasco <70547315+erika-carrasco@users.noreply.github.com>
2020-10-28 12:29:33 -0600

    Add files via upload

---
+++ reto 2.sql
@@ -0,0 +1,25 @@
+SHOW DATABASES;
+USE tienda;
+SHOW TABLES;
+
+DESCRIBE empleado;
+SELECT nombre 
+FROM empleado 
+WHERE id_puesto=4;
+
+DESCRIBE puesto;
+SELECT* 
+FROM puesto 
+WHERE salario>10000;
+
+DESCRIBE articulo;
+SELECT* 
+FROM articulo 
+WHERE precio>1000 
+	AND iva>100;
+
+DESCRIBE venta;
+SELECT* 
+FROM venta 
+WHERE id_articulo IN (135,963) 
+	AND id_empleado IN (835,369);
